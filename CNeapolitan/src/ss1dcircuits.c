441b1f6316989931e98602db26bb13d1d1ca1e90
Grant <christiansengrant18@gmail.com>
2024-03-14 22:18:38 -0500

    replaced functions with macro invocation

--- CNeapolitan/src/ss1dcircuits.c
+++ CNeapolitan/src/ss1dcircuits.c
@@ -1,45 +1,14 @@
 #include "ss1dcircuits.h"
 
-struct Node_T* newNode1D()
-{
-    Node_T* node = (Node_T*)malloc(sizeof(Node_T) + sizeof(double));
-    
-    node->inputs = newPtrVec();
-    node->outputs = newPtrVec();
-    node->potential[0] = 1;
-    node->isLocked = 0;
-    
-    return node;
-}
+ELEMENT_CONSTRUCTOR(resistor, RESISTOR, 1)
 
-int addNode1D(Problem_T* problem)
-{
-    Problem_T* maybeNew = pushValToPtrVec(problem, newNode1D());
-    if (maybeNew == NULL)
-    {
-        return ERR;
-    }
-    problem = maybeNew;
-    return 1;
-}
+ELEMENT_CONSTRUCTOR(voltageSourceHelper, VOLTAGESOURCE, 1)
 
-int resistor(Problem_T problem, size_t a, size_t b)
-{
-    return newBasicElement(
-        problem, 
-        RESISTOR, 
-        (problem.nodes + a),
-        (problem.nodes + b),
-        (size_t)1);
-}
+ELEMENT_CONSTRUCTOR(currentSourceHelper, CURRENTSOURCE, 1)
 
-int voltageSource(Problem_T problem, size_t a, size_t b)
+int voltageSource(Problem_T* problem, size_t a, size_t b)
 {
-    // Cannot connect a node to itself 
-    if (a == b)
-    {
-        return ERR;
-    }
-    // Push element to element vector
-    ElementVec_T* tmp;
+    ABORT_IF_NULL(voltageSourceHelper(problem, a, b));
+
+    problem->elements
 }
