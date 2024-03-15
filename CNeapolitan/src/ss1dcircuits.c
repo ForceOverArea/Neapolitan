e739a540b51c80f7d5efb57004fdd4a121cb8912
Grant <christiansengrant18@gmail.com>
2024-03-14 22:37:24 -0500

    still going at it

--- CNeapolitan/src/ss1dcircuits.c
+++ CNeapolitan/src/ss1dcircuits.c
@@ -1,14 +1,57 @@
 #include "ss1dcircuits.h"
 
-ELEMENT_CONSTRUCTOR(resistor, RESISTOR, 1)
+//                  Function Name           Element Kind    Gain Dimension
+//                  |                       |               |
+//                  V                       V               V
+ELEMENT_CONSTRUCTOR(resistorHelper,         RESISTOR,       1)
+ELEMENT_CONSTRUCTOR(voltageSourceHelper,    VOLTAGESOURCE,  1)
+ELEMENT_CONSTRUCTOR(currentSourceHelper,    CURRENTSOURCE,  1)
 
-ELEMENT_CONSTRUCTOR(voltageSourceHelper, VOLTAGESOURCE, 1)
-
-ELEMENT_CONSTRUCTOR(currentSourceHelper, CURRENTSOURCE, 1)
-
-int voltageSource(Problem_T* problem, size_t a, size_t b)
+int resistor(Problem_T* problem, size_t a, size_t b, double resistance)
 {
-    ABORT_IF_NULL(voltageSourceHelper(problem, a, b));
+    ABORT_IF_NULL(voltageSourceHelper(problem, a, b))
+    
+    size_t back = problem->elements->len;
+    Element_T* lastElem = &problem->elements->data[back];
+    lastElem->gain[0] = resistance;
 
-    problem->elements
+    return OK;
+}
+
+int voltageSource(Problem_T* problem, size_t i, size_t o, double voltage)
+{
+    ABORT_IF_NULL(voltageSourceHelper(problem, i, o))
+    
+    size_t back = problem->elements->len;
+    Element_T* lastElem = &problem->elements->data[back];
+    lastElem->gain[0] = voltage;
+
+    Node_T* input = problem->nodes->data[i].pVal;
+    Node_T* output = problem->nodes->data[o].pVal;
+
+    if (!output->isLocked)
+    {
+        output->isLocked = TRUE;
+    }
+    else if (output->isLocked && !input->isLocked)
+    {
+        input->isLocked = TRUE;
+    }
+    else
+    {
+        return ERR;
+    }
+
+    return OK;
+}
+
+int currentSource(Problem_T* problem, size_t a, size_t b, double current)
+{
+    ABORT_IF_NULL(voltageSourceHelper(problem, a, b))
+    
+    size_t back = problem->elements->len;
+    Element_T* lastElem = &problem->elements->data[back];
+    lastElem->gain[0] = current;
+
+    return OK;
 }
