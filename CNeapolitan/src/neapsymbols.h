ade71d67c55dc90ab5bad4443078da2fd811e5bf
Grant <christiansengrant18@gmail.com>
2024-03-15 22:51:30 -0500

    added more docstrings

--- CNeapolitan/src/neapsymbols.h
+++ CNeapolitan/src/neapsymbols.h
@@ -11,20 +11,22 @@ const double DX = _DX_VALUE;
 /// approximating improper integrals that are assumed to converge.  
 const double DISCRETE_INF = 1 / _DX_VALUE; 
 
-/// @brief Return value inticating success on "try-routine" pattern functions
+/// @brief Return value inticating process success
 #define OK 1
 
-/// @brief Return value inticating failure on "try-routine" pattern functions
+/// @brief Return value inticating process failure
 #define ERR 0
 
+/// @brief Alias for 1 for better legibility
 #define TRUE 1
 
+///@brief Alias for 1 for better legibility
 #define FALSE 0
 
 /// @brief Causes a wrapping function to return 
 /// `ERR` if the given pointer is `NULL`.
 #define ABORT_IF_NULL(var)  \
-if (NULL == (var))          \
+if (NULL == (void*)(long long)(var))\
 {                           \
     return ERR;             \
 }                   
